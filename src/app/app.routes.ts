dca2b56182ad746a53d650029665dc95dbd9a6fa
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-06-22 15:38:38 +0300

    add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,12 @@ export const routes: Routes = [
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
+  },
+  //  Catch-all route for 404 Not Found
+  // This should be the last route in the array
+  // to ensure it only matches when no other routes do.
+  {
+    path: '**',
+    component: NotFound
   }
 ];
