3aab4efe3110d814c595826df4696a40343bc694
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-06-23 14:01:02 +0300

    example for configuring API route

--- estore-server/index.js
+++ estore-server/index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 5001;
 
+app.get("/", (req, res) => {
+  let prodData = {
+    pName: "Jackets",
+    price: 45,
+    img: "shop-1.jpg",
+  };
+  res.status(200).send(prodData);
+});
+
 const server = app.listen(PORT, () => {
   console.log("App is running on the port - 5001");
 });
