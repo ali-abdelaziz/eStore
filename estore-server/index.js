6619dd729fffc8c85631b4ee1a09cbc9759acbe9
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-06-24 12:39:10 +0300

    create routes with Express

--- estore-server/index.js
+++ estore-server/index.js
@@ -1,26 +1,9 @@
 const express = require("express");
-const mysql = require("mysql2");
+const productCategories = require("./routes/productCategories");
 const app = express();
 const PORT = 5001;
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Ali@1234",
-  database: "estore",
-  port: 3306,
-  multipleStatements: true,
-});
-
-app.get("/", (req, res) => {
-  pool.query("SELECT * FROM products", (error, results) => {
-    if (error) {
-      res.status(500).send(error);
-    } else {
-      res.status(200).json(results);
-    }
-  });
-});
+app.use("/productCategories", productCategories);
 
 const server = app.listen(PORT, () => {
   console.log("App is running on the port - 5001");
