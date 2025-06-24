4a34dd3a5fbe8b83775fa5c3893bdf0bf2b27c7f
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-06-24 15:40:38 +0300

    fetch categories in category navigation bar

--- estore-client/src/app/components/home/catnavigation/catnavigation.ts
+++ estore-client/src/app/components/home/catnavigation/catnavigation.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Category } from '../types/category';
+import { CategoryService } from '../services/category';
 
 @Component({
   selector: 'app-catnavigation',
   imports: [],
   templateUrl: './catnavigation.html',
-  styleUrl: './catnavigation.css'
+  styleUrl: './catnavigation.css',
 })
 export class Catnavigation {
+  categories: Category[] = [];
 
+  constructor(private categoryService: CategoryService) {
+    this.categoryService.getAllCategories().subscribe((categories) => {
+      // Filter categories to only include top-level categories (return only categories where parent_category_id is null)
+      this.categories = categories.filter(
+        (category) => category.parent_category_id === null
+      );
+      console.log('Categories:', this.categories);
+    });
+  }
 }
