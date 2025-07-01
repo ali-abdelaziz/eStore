e76c9cda92fef809f5b907ea91a3aaf75348d8e6
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-07-01 15:15:53 +0300

    make the UI dynamic on route change - product-details page

--- estore-client/src/app/components/home/catnavigation/catnavigation.ts
+++ estore-client/src/app/components/home/catnavigation/catnavigation.ts
@@ -1,7 +1,8 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { Category } from '../types/category';
 import { CategoriesStoreItem } from '../services/category/categories.storeItem';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-catnavigation',
@@ -11,7 +12,17 @@ import { RouterLink } from '@angular/router';
 })
 export class Catnavigation {
   readonly categoryClicked = output<number>();
-  constructor(public categoryStore: CategoriesStoreItem) {}
+  displayOptions = signal(true);
+
+  constructor(public categoryStore: CategoriesStoreItem, private router: Router) {
+    this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    )
+    .subscribe((event: NavigationEnd) => {
+      this.displayOptions.set(event.url === '/home/products');
+    })
+  }
 
   onCategoryClick(mainCategory: Category): void {
     this.categoryClicked.emit(mainCategory.id);
--- estore-client/src/app/components/home/header/header.ts
+++ estore-client/src/app/components/home/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
   faSearch,
@@ -7,7 +7,8 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { CategoriesStoreItem } from '../services/category/categories.storeItem';
 import { SearchKeyword } from '../types/searchKeyword.type';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -21,8 +22,17 @@ export class Header {
   faShoppingCart = faShoppingCart;
 
   readonly searchClicked = output<SearchKeyword>();
+  displaySearch = signal(true);
 
-  constructor(public categoryStore: CategoriesStoreItem) {}
+  constructor(public categoryStore: CategoriesStoreItem, private router: Router) {
+    this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    )
+    .subscribe((event: NavigationEnd) => {
+      this.displaySearch.set(event.url === '/home/products');
+    })
+  }
 
   onClickSearch(keyword: string, categoryId: string): void {
     this.searchClicked.emit({
