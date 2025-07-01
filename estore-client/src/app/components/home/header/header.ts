68a59342a6f0c3f140afa52af116633acb6a5c36
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-07-01 11:11:55 +0300

    find products using categories filter and search box

--- estore-client/src/app/components/home/header/header.ts
+++ estore-client/src/app/components/home/header/header.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faSearch, faShoppingCart, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSearch,
+  faShoppingCart,
+  faUserCircle,
+} from '@fortawesome/free-solid-svg-icons';
 import { CategoriesStoreItem } from '../services/category/categories.storeItem';
+import { SearchKeyword } from '../types/searchKeyword.type';
 
 @Component({
   selector: 'app-header',
   imports: [FontAwesomeModule],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
 })
 export class Header {
   faSearch = faSearch;
   faUserCircle = faUserCircle;
   faShoppingCart = faShoppingCart;
 
+  readonly searchClicked = output<SearchKeyword>();
+
   constructor(public categoryStore: CategoriesStoreItem) {}
+
+  onClickSearch(keyword: string, categoryId: string): void {
+    this.searchClicked.emit({
+      categoryId: parseInt(categoryId),
+      keyword: keyword
+    });
+  }
 }
--- estore-client/src/app/components/home/home.ts
+++ estore-client/src/app/components/home/home.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
-import { Header } from "./header/header";
-import { Catnavigation } from "./catnavigation/catnavigation";
-import { Sidenavigation } from "./sidenavigation/sidenavigation";
-import { Products } from "../products/products";
+import { Header } from './header/header';
+import { Catnavigation } from './catnavigation/catnavigation';
+import { Sidenavigation } from './sidenavigation/sidenavigation';
+import { Products } from '../products/products';
 import { CategoriesStoreItem } from './services/category/categories.storeItem';
 import { CategoryService } from './services/category/category';
 import { ProductsStoreItem } from '../products/services/products.storeItem';
 import { ProductsService } from '../products/services/products';
-
+import { SearchKeyword } from './types/searchKeyword.type';
 
 @Component({
   selector: 'app-home',
   imports: [Header, Catnavigation, Sidenavigation, Products],
   templateUrl: './home.html',
   styleUrl: './home.css',
-  providers: [CategoryService, CategoriesStoreItem, ProductsStoreItem, ProductsService],
+  providers: [
+    CategoryService,
+    CategoriesStoreItem,
+    ProductsStoreItem,
+    ProductsService,
+  ],
 })
 export class Home {
   constructor(
@@ -31,5 +36,12 @@ export class Home {
 
   onSelectCategory(mainCategoryId: number): void {
     this.productsStoreItem.loadProducts({ maincategoryid: mainCategoryId });
+  }
+
+  onSearchKeyword(searchKeyword: SearchKeyword): void {
+    this.productsStoreItem.loadProducts({
+      maincategoryid: searchKeyword.categoryId,
+      keyword: searchKeyword.keyword
+    });
   }
 }
