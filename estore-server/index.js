33f35436ad688e5e411893d0ed831ec53c0732c6
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2025-07-09 12:45:05 +0300

    create user signup api

--- estore-server/index.js
+++ estore-server/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const productCategories = require("./routes/productCategories");
 const products = require("./routes/products");
 const cors = require("cors");
+const user = require("./routes/users");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5001;
 
 app.use(cors()); // Enable CORS for all routes to avoid CORS issues
+app.use(bodyParser.json()); // Parse JSON bodies [parse every incoming request with JSON payloads into req.body]
 
 app.use("/productCategories", productCategories);
 app.use("/products", products);
+app.use("/users", user);
 
 const server = app.listen(PORT, () => {
   console.log("App is running on the port - 5001");
